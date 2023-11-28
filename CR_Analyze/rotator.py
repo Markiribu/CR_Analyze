a7764e85d7dce153b0eff527bf1deeacbe848542
Marcos Bugueño <marcoseduardo.castillo@userena.cl>
2023-11-28 09:42:31 -0300

    Updated rotator.py with the rotation function, and begun the coding of the multiple rotation algorithm.

--- CR_Analyze/rotator.py
+++ CR_Analyze/rotator.py
@@ -53,94 +53,47 @@ def matrix_from_spherical( r, theta, phi):
     M = np.dot(Ry,Rz)
     return(M)
 
-def star_particles_rotated_once_eulermethod(subhaloID,snapNum,basepath,radius_limit,minmetal,maxmetal,snap_header):
+def table_rotate(table,rotation_matrix):
     """
-    Loads and rotates the particles of a given subhalo, it requires an object named snap_header in a global context, that corrects comoving systems to physical
+    Rotates the table of xyz coordinates by the given matrix.
     Parameters:
-    - subhaloID (int) The identication of the subhalo
-    - snapNum (int) The snapshot number
-    - basepath (str) Directory in which the simulation data is allocated
-    - radius_limit (float) maximum distance to the center in kpc of the rotation reference particles
-    - minmetal (float) minimum metallicity in solar metallicity units for the reference particles
-    - maxmetal (float) maximum metallicity in solar metallicity units for the reference particles
-    - snap_header (pd.DataFrame) DataFrame containing information about the snapshots like scalefactor, redshifts, h, etc.
+    - table (array) an array of shape (N,3)
+    - rotation_matrix (numpy.Matrix) the rotation matrix
     Returns:
-    - stars (pd.DataFrame)
-    - M (np.Matrix)
+    - table (array) the array of shape (N,3) multiplied by the rotation matrix
     """
-    #ROTACION MEDIANTE METODO DE MOMENTO ANGULAR
-    #constantes
-    redshift_z = np.round(snap_header['redshift'][snapNum-1],3)
-    scalefactor_a = snap_header['scalefactor'][snapNum-1]
-    
-    #esto no es necesario, ya que h y los omegas son valores fijos en verdad, (._.)
-    h = snap_header['h'][snapNum-1]
-    H_0 = h/10
-    omega_0 = snap_header['omega_0'][snapNum-1]
-    omega_lambda = snap_header['omega_lambda'][snapNum-1]
-    
-    w_a = omega_0/(omega_lambda*(scalefactor_a**3))
-    H_a = H_0 * np.sqrt(omega_lambda) * np.sqrt(1 + w_a)
-    
-    campos = ['Masses','Coordinates','Velocities','GFM_Metallicity','Potential','GFM_StellarFormationTime','ParticleIDs']
-    
-    #carga de datos
-    stars = il.snapshot.loadSubhalo(basepath, snapNum, subhaloID, 'star', fields=campos)
-    subhalo = il.groupcat.loadSingle(basepath, snapNum, subhaloID=subhaloID)
-    
-    shPos = subhalo['SubhaloPos']
-    rHalf = subhalo['SubhaloHalfmassRadType'][4]
-    shVel = subhalo['SubhaloVel']
-    
-    #cambio unidades de las particulas a terminos fisicos
-    stars['Masses'] *= (1e10/h)
-    stars['Coordinates'] = stars['Coordinates']*(scalefactor_a/h)
-    stars['GFM_Metallicity'] /= 0.0127
-    stars['Velocities'] *= np.sqrt(scalefactor_a)
-    stars['Velocities'] = H_a * stars['Coordinates'] + stars['Velocities']
-    
-    
-    rHalf *= (scalefactor_a/h)
-    shPos *= (scalefactor_a/h)
-    
-    #velocidad peculiar a fisica
-    shVel =  H_a * shPos + shVel
-    
-    
-    
-    print(rHalf)
-    
-    #distancias al centro
-    vector_desde_centro = stars['Coordinates'] - shPos
-    stars['Distance_to_center'] = np.array([np.linalg.norm(v) for v in vector_desde_centro])
-    
-    #filtrando por distancia al centro y por metalicidad similar a la solar
-    wStars = np.where( (stars['GFM_Metallicity'] >= minmetal) & (stars['GFM_Metallicity'] <= maxmetal) & (stars['Distance_to_center'] <= radius_limit) & (stars['GFM_StellarFormationTime'] > 0) )
-    stars['Masses'] = stars['Masses'][wStars]
-    stars['Coordinates'] = stars['Coordinates'][wStars]
-    stars['GFM_Metallicity'] = stars['GFM_Metallicity'][wStars]
-    stars['Distance_to_center'] = stars['Distance_to_center'][wStars]
-    stars['Velocities'] = stars['Velocities'][wStars]
-    stars['Potential'] = stars['Potential'][wStars]
-    stars['ParticleIDs'] = stars['ParticleIDs'][wStars]
-    stars['count'] = len(stars['Masses'])
-    
-    #cambio de marco de referencia
-    stars['Coordinates'] -= shPos
-    stars['Velocities'] -= shVel
-    
-    
-    #calculo del momento angular
-    stars['Angular_Momentum'] = np.cross(stars['Coordinates'],stars['Velocities'])
-    total_angular_momentum = sum(stars['Angular_Momentum'])
-    
-    r, theta, phi = spherical_coords_from_vector(total_angular_momentum)
-    M = matrix_from_spherical(r, theta, phi)
-    print(np.dot(M, total_angular_momentum))
-    
-    
-    stars['Coordinates'] = np.array([np.array(np.dot(M, Coord))[0] for Coord in stars['Coordinates']])
-    stars['Velocities'] = np.array([np.array(np.dot(M, Velocidad))[0] for Velocidad in stars['Velocities']])
-    
-    return(stars, M)
+    for i in range(len(table)):
+        xyz = table[i]
+        xyz = np.dot(rotation_matrix, xyz)
+        table[i] = xyz
+    return(table)
 
+def table_rotated_once_angularmomenta(tabla, reference_tabla, debug=False):
+    """
+    Rotates a table of particles by using a given reference table. Both must be in dict form with values of "Velocities" and "Coordinates"
+    Parameters:
+    - tabla (dict) a dictionary with the form {"Coordinates":numpy.array(N,3),"Velocities":numpy.array(3,N)}
+    - reference_tabla (dict) a dictionary with the form {"Coordinates":numpy.array(N,3),"Velocities":numpy.array(3,N)}
+    Returns:
+    - tabla (dict), the same dictionary but with its coordinates and velocities rotated
+    - M (np.array(3,3)) the rotation matrix calculated and used. ideally for later saving.
+    """
+    # We assume that the conversion to physical coordinates has been done already, as such we calculate the angular momenta from the reference table that is going to be used to rotate
+    # We assume as well that (0,0,0) would be the center of the subhalo or galaxy
+    reference_tabla["Angular_Momentum"] = np.cross(reference_tabla["Coordinates"],reference_tabla["Velocities"])
+    reference_J = sum(reference_tabla["Angular_Momentum"]) #Referential angular momentum
+    r, theta, phi = spherical_coords_from_vector(reference_J) #Angles of rotation
+    M = matrix_from_spherical(r, theta, phi)
+    if debug: print(np.dot(M, reference_J)) #In case of debugging, check rotated angular momentum.
+
+    #Rotate the table "tabla" by the reference matrix
+    tabla["Coordinates"] = table_rotate(tabla["Coordinates"], M)
+    tabla["Velocities"] = table_rotate(tabla["Velocities"],M)
+
+    return(tabla,M)
+
+def table_rotated_n_angularmomenta(tabla,debug=False):
+    """
+    Rotates the table multiple times trough an algorithm of consecutive smaller spheres, it assumes that tabla contains the LMAO sigo escribiendo de ahi
+    """
+    return(tabla)
