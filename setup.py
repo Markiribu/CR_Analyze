3a3fdb2cafeccdafbea41bb8b636fb2163a7038a
Markiribu <marcoseduardo.castillo@userena.cl>
2024-09-03 11:19:02 -0400

    Fixed bad comma in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='CR_Analyze',
     version='0.0.2',
     packages=['CR_Analyze'],
-    install_requires=['numpy','matplotlib']
+    install_requires=['numpy','matplotlib'],
     extras_require={
             "illustristng": ["h5py","illustris_python"]
         }
